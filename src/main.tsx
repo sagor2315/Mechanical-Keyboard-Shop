9a256ae08de3e6cfa6488a7c1bf4d44ad76c924b
Zahidul Islam  Sagor <117823487+sagor2315@users.noreply.github.com>
2024-08-28 16:27:55 +0600

    modified some ui components and files for remove type error

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes.tsx";
-// import { ThemeProvider } from "./components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> */}
       <RouterProvider router={router} />
-      {/* </ThemeProvider> */}
     </Provider>
   </React.StrictMode>
 );
